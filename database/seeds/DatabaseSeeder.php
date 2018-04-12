bc97e4c5d8c08e895ac2522cdeb4f244c2cb5deb
Juanjo <jjcadu@hotmail.com>
2018-04-11 23:11:05 -0500

    Modificación del seeder de usuarios

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run() {
         DB::table('users')->insert([
-            'name' => 'Corona',
-            'password' => bcrypt('admin'),
+            'name' => 'test',
+            'password' => 'admin',
         ]);
     }
 }
